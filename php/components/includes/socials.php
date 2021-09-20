01e40cffe759312aa255b7b92eb9f86ea06f1cb2
Lubricant Jam <LubricantJam@users.noreply.github.com>
2021-09-20 21:08:51 +0100

    Removed all old code.

--- php/components/includes/socials.php
+++
@@ -1,3 +0,0 @@
-<span class="mr-2">Follow me on</span>
-<a href="https://github.com/LubricantJam/" class="text-decoration-none ml-3"><i class="bi-github"></i></a>
-<a href="https://www.linkedin.com/in/james-david-finch-a3564615b/" class="text-decoration-none ml-3"><i class="bi-linkedin"></i></a>
