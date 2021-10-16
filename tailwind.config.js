c66087e254058ce309f25c6eb5f6e46550bc8cb1
Lubricant Jam <LubricantJam@users.noreply.github.com>
2021-10-16 19:32:26 +0100

    Added Skills Section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     colors: {
       transparent: 'transparent',
-      white: '#FFFFFF',
+      white: '#FFFC',
       yellow: '#EBCB8B',
       grey: {
         DEFAULT: '#2E3440',
